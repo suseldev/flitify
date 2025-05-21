b6b39c96c200f6607b6afed7d3f93de38f364660
xxSusel <xxsusel@protonmail.com>
2025-05-21 19:55:58 +0200

    add basic key generation utility

--- app/crypto/utils/rsakeyutils.py
+++ app/crypto/utils/rsakeyutils.py
@@ -1,4 +1,6 @@
 from Crypto.PublicKey import RSA
+import os
+import sys
 
 def generateRsaKeypair(privKeyTargetPath: str, pubKeyTargetPath: str):
     keyPair = RSA.generate(4096)
@@ -18,3 +20,17 @@ def loadKeyFromFile(pubKeyFilePath: str) -> bytes:
     keyF.close()
     return key
 
+def main():
+    if len(sys.argv) < 2:
+        print(f"Usage: python {sys.argv[0]} <directory to store RSA keys>")
+        sys.exit(1)
+    dirname = sys.argv[1]
+    if os.path.isdir(dirname):
+        print("Generating RSA keys...")
+        generateRsaKeypair(dirname + '/priv_key.pem', dirname + '/pub_key.pem')
+        print("RSA keys generated")
+    else:
+        print(f"Invalid path: {dirname}")
+
+if __name__ == "__main__":
+    main()
