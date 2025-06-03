19c709ea9df9ba1e7b58e0e2c18bd6f5b6a4a390
xxSusel <xxsusel@protonmail.com>
2025-06-03 22:37:34 +0200

    Config example changes

---
+++ docker/generate_all_config.py
@@ -0,0 +1,62 @@
+import json
+import secrets
+import string
+from pathlib import Path
+
+def random_str(length=16):
+    chars = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(chars) for _ in range(length))
+
+def token_hex(length=32):
+    return secrets.token_hex(length // 2)
+
+MONGO_USER = random_str(8)
+MONGO_PASS = random_str(16)
+API_SECRET = token_hex(64)
+JWT_SECRET = token_hex(64)
+
+# git repo root directory
+BASE = Path(__file__).resolve().parent.parent
+
+SERVER_CFG_TEMPLATE = BASE / "app" / "config_server_example.json"
+BACKEND_CFG_TEMPLATE = BASE / "flitify-webpanel" / "backend" / "config_backend_example.json"
+SERVER_CFG = BASE / "app" / "config_server.json"
+BACKEND_CFG = BASE / "flitify-webpanel" / "backend" / "config_backend.json"
+ENV_PATH = BASE / ".env.generated"
+
+
+with open(SERVER_CFG_TEMPLATE) as f:
+    server_cfg = json.load(f)
+
+print(server_cfg)
+server_cfg["flitify_server"]["db_user"] = MONGO_USER
+server_cfg["flitify_server"]["db_password"] = MONGO_PASS
+server_cfg["flitify_server"]["db_address"] = "mongo:27017" # docker hostname
+server_cfg["api_server"]["secret"] = API_SECRET
+
+with open(SERVER_CFG, "w") as f:
+    json.dump(server_cfg, f, indent=4)
+
+with open(BACKEND_CFG_TEMPLATE) as f:
+    backend_cfg = json.load(f)
+
+backend_cfg["db_user"] = MONGO_USER
+backend_cfg["db_password"] = MONGO_PASS
+backend_cfg["db_address"] = "mongo:27017"
+backend_cfg["internal_api_secret"] = API_SECRET
+backend_cfg["jwt_secret"] = JWT_SECRET
+
+with open(BACKEND_CFG, "w") as f:
+    json.dump(backend_cfg, f, indent=4)
+
+with open(ENV_PATH, "w") as f:
+    f.write(f"MONGO_USER={MONGO_USER}\n")
+    f.write(f"MONGO_PASS={MONGO_PASS}\n")
+    f.write(f"API_SECRET={API_SECRET}\n")
+    f.write(f"JWT_SECRET={JWT_SECRET}\n")
+
+print(f"""[+] Config generated and saved to: 
+FlitifyServer: {SERVER_CFG}
+FlitifyWebPanel Backend: {BACKEND_CFG}
+""")
+
